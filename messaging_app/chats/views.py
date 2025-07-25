45b2a9d066d22fada0d1739b2a35cf9efba32480
Brianryanphiri <105008060+Brianryanphiri@users.noreply.github.com>
2025-07-25 10:03:08 +0000

    Add IsAuthenticated and custom permission IsParticipantOfConversation to MessageViewSet with proper access control and 403 handling

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,23 +1,30 @@
-from rest_framework import viewsets, status
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Message
 from .serializers import MessageSerializer
 from .permissions import IsParticipantOfConversation
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsParticipantOfConversation]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
-        conversation_id = self.request.query_params.get('conversation_id')
-        if not conversation_id:
-            raise PermissionDenied(detail="conversation_id is required.", code=status.HTTP_403_FORBIDDEN)
-        return Message.objects.filter(conversation_id=conversation_id, conversation__participants=self.request.user)
+        # Assuming Message has a conversation field with participants relation
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
-        conversation_id = self.request.data.get('conversation')
-        if not conversation_id:
-            raise PermissionDenied(detail="conversation is required.", code=status.HTTP_403_FORBIDDEN)
-        # Optionally, add more permission checks here
         serializer.save(owner=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)

afc10cb6a28a1c98136fed9f30f95fc9285a5167
Brianryanphiri <105008060+Brianryanphiri@users.noreply.github.com>
2025-07-25 11:56:06 +0000

    Implement custom permissions and enforce access control in MessageViewSet

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,8 +1,7 @@
-# chats/views.py
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
+from rest_framework.exceptions import PermissionDenied
 from .models import Message
 from .serializers import MessageSerializer
 from .permissions import IsParticipantOfConversation
@@ -12,18 +11,28 @@ class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
-        # Only return messages from conversations the user participates in
-        return Message.objects.filter(conversation__participants=self.request.user)
+        conversation_id = self.request.query_params.get('conversation_id')
+        if conversation_id:
+            return Message.objects.filter(
+                conversation__id=conversation_id,
+                conversation__participants=self.request.user
+            )
+        return Message.objects.none()
 
     def perform_create(self, serializer):
+        conversation = serializer.validated_data.get('conversation')
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied("You are not a participant in this conversation.")
         serializer.save(owner=self.request.user)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
-        self.check_object_permissions(request, instance)  # <--- Triggers custom permission
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-        self.check_object_permissions(request, instance)  # <--- Triggers custom permission
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
         return super().destroy(request, *args, **kwargs)
